dfab5bd08d4c66234817f0ea3c55c6cc1e0d3292
Peregr1ne <champvd225@hotmail.com>
2026-01-18 23:23:07 +0000

    Add Split text with type to html

---
+++ src/inline.py
@@ -0,0 +1,27 @@
+from typing import Text
+
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+        if delimiter not in old_node.text:
+            raise ValueError(f"Delimiter '{delimiter}' not found in text node")
+        delimiter_count = old_node.text.count(delimiter)
+        if delimiter_count % 2 != 0:
+            raise ValueError(f"Delimiter '{delimiter}' not balanced in text node")
+        new_node = []
+        parts = old_node.text.split(delimiter)
+        for i, part in enumerate(parts):
+            if part == "":
+                continue
+            if i % 2 != 0:
+                new_node.append(TextNode(part, text_type))
+            else:
+                new_node.append(TextNode(part, TextType.TEXT))
+        new_nodes.extend(new_node)
+    print(new_nodes)
+    return new_nodes
---
+++ src/test_splitdelimiter.py
@@ -0,0 +1,43 @@
+import unittest
+
+from inline import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_split_delimiter_with_not_matching_delimiter(self):
+        text = "`Hello`,` World!"
+        delimiter = "`"
+        node = TextNode(text, TextType.TEXT)
+        with self.assertRaises(ValueError) as context:
+            split_nodes_delimiter([node], delimiter, TextType.TEXT)
+        self.assertEqual(
+            str(context.exception),
+            "Delimiter '`' not balanced in text node",
+        )
+
+    def test_split_delimiter_with_empty_text(self):
+        text = ""
+        delimiter = "`"
+        node = TextNode(text, TextType.TEXT)
+        with self.assertRaises(ValueError) as context:
+            split_nodes_delimiter([node], delimiter, TextType.TEXT)
+        self.assertEqual(
+            str(context.exception),
+            "Delimiter '`' not found in text node",
+        )
+
+    def test_split_delimiter_with_multiple_delimiters(self):
+        text = "**Hello**, World!, **It's me**"
+        delimiter = "**"
+        expected = [
+            TextNode("Hello", TextType.BOLD),
+            TextNode(", World!, ", TextType.TEXT),
+            TextNode("It's me", TextType.BOLD),
+        ]
+        self.assertEqual(
+            split_nodes_delimiter(
+                [TextNode(text, TextType.TEXT)], delimiter, TextType.BOLD
+            ),
+            expected,
+        )
