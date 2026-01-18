b0565175e2b1e95579fdb8ff45cb27e8ddc47337
Peregr1ne <champvd225@hotmail.com>
2026-01-18 21:11:33 +0000

    Add function to change text to html

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from typing import Text
+
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -27,3 +30,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(node):
+    if node.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=node.text)
+    elif node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=node.text)
+    elif node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=node.text)
+    elif node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=node.text)
+    elif node.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=node.text, props={"href": node.url})
+    elif node.text_type == TextType.IMAGE:
+        return LeafNode(tag="img", value="", props={"src": node.url, "alt": node.text})
+    else:
+        raise ValueError(f"Unknown text type: {node.text_type}")
