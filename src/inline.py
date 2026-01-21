3a87cf4f38284c7825c036491a34de8dc147eb36
Peregr1ne <champvd225@hotmail.com>
2026-01-21 02:36:00 +0000

    Update inline.py

--- src/inline.py
+++ src/inline.py
@@ -96,11 +96,3 @@ def text_to_textnodes(text):
     node = split_nodes_image(node)
     node = split_nodes_link(node)
     return node
-
-
-if __name__ == "__main__":
-    print(
-        text_to_textnodes(
-            "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-        )
-    )
